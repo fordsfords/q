1129097bd3b692f01df1cf84fe120a824b36d556
fordsfords <sford@geeky-boy.com>
2015-05-12 20:20:36 -0500

    doc
    Minor updates to doc; process with latest semlit

--- bld_semlit.sh
+++ bld_semlit.sh
@@ -15,20 +15,23 @@ echo "FYI cd html"
 cd html
 echo "FYI cp ../doc/*.png ."
 cp ../doc/*.png .
+
 echo "FYI semlit.sh -I../doc ../doc/q.sldoc"
 if semlit.sh -I../doc ../doc/q.sldoc; then :
-else echo "fail"
+else echo "fail semlit"
 fi
+
 # Remove execute permission so web servers don't refuse to serve it.
 chmod -x *.txt
+
 # We've generated fresh .c and .h files.  Copy them into the project dir.
 for F in *_c.txt; do f=`basename $F _c.txt`;
-	echo "FYI rm -f ../$f.c; cp $F ../$f.c; chmod -w ../$f.c"
-	rm -f ../$f.c; cp $F ../$f.c; chmod -w ../$f.c
+  echo "FYI rm -f ../$f.c; cp $F ../$f.c; chmod -w ../$f.c"
+  rm -f ../$f.c; cp $F ../$f.c; chmod -w ../$f.c
 done
 for F in *_h.txt; do f=`basename $F _h.txt`;
-	echo "FYI rm -f ../$f.h; cp $F ../$f.h; chmod -w ../$f.h"
-	rm -f ../$f.h; cp $F ../$f.h; chmod -w ../$f.h
+  echo "FYI rm -f ../$f.h; cp $F ../$f.h; chmod -w ../$f.h"
+  rm -f ../$f.h; cp $F ../$f.h; chmod -w ../$f.h
 done
 
 echo "FYI cd .."
